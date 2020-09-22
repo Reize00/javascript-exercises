6d4c90d72d7937cbef3734c616f9463e1848c7c2
Wesslee Whatley <wesslee.whatley@gmail.com>
2020-09-22 17:50:42 -0500

    Complete palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    //remove all punctuation
+    str = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+    const forwardArray = str.split("");
+    const backwardArray = [];
+
+    for (x = 0; x < forwardArray.length; x++) {
+        backwardArray.unshift(forwardArray[x]);
+    }
+
+    strA = JSON.stringify(forwardArray).toLowerCase();
+    console.log(strA);
+    strB = JSON.stringify(backwardArray).toLowerCase();
+    console.log(strB);
+    
+    if(JSON.stringify(forwardArray).toLowerCase() == JSON.stringify(backwardArray).toLowerCase()) {
+        return true;
+    } else return false;
 
 }
 
