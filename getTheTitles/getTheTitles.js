aa98058734af4a8e6ba6f6e4dcfeab803df054b1
Wesslee Whatley <wesslee.whatley@gmail.com>
2020-09-25 19:04:30 -0500

    Complete getTheTitles exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    answer = [];
+    for (book in books) {
+        answer.push(books[book].title);
+    }
+    return answer;
 }
 
 module.exports = getTheTitles;
