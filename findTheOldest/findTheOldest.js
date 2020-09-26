3451e9e4a7b60ab573386951a63369eddcce51d6
Wesslee Whatley <wesslee.whatley@gmail.com>
2020-09-25 19:29:42 -0500

    Complete findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-let findTheOldest = function() {
+function getAge(person) {
+    let date = new Date();
+    let age = 0;
+    if(person.yearOfBirth === "undefined") {
+        age = 0;
+    } else if("yearOfDeath" in person) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        age = date.getFullYear() - person.yearOfBirth;
+    }
+    return age;
+}
 
+let findTheOldest = function(people) {
+    currentOldest = {
+        name: 'test',
+        yearOfBirth: 2020,
+    };
+    for (person in people) {
+        if (getAge(people[person]) > getAge(currentOldest)) {
+            currentOldest = people[person];
+        }
+    }
+
+    return currentOldest;
 }
 
 module.exports = findTheOldest
